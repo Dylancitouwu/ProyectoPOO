df081629a1f6c885c5b085961d50270ddc8d9703
Dylancitouwu <daguilarq@est.ups.edu.ec>
2024-07-18 18:04:11 -0500

    Delete Proyectoestructura directory

--- Proyectoestructura/sources/Boleto.cpp
+++
@@ -1,8 +0,0 @@
-#include "../headers/Boleto.h"
-
-Boleto::Boleto(int sala, Pelicula pelicula, QString fila, int columna)
-    : sala(sala), pelicula(pelicula), fila(fila), columna(columna) {}
-
-QString Boleto::getAsiento() const {
-    return fila + QString::number(columna);
-}
